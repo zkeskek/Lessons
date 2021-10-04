25945ed98ade921f6d5934a9d1a7a96acf0a753b
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-04 10:58:01 +0300

    kdv tutarı hesapla

---
+++ Java/Patika/KDVTutariHesapla/src/KDVTutariHesapla.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class KDVTutariHesapla {
+    // https://app.patika.dev/moduller/java101/pratik-kdv-hesaplama
+    public static void main(String[] args) {
+        final double vatRate = 0.18;
+        double priceWithoutVat;
+        double priceWithVat;
+
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            System.out.print("Fiyat Giriniz : ");
+            priceWithoutVat = scanner.nextDouble();
+
+            priceWithVat = priceWithoutVat * vatRate + priceWithoutVat;
+
+            System.out.println("KDV'siz fiyat : " + priceWithoutVat);
+            System.out.println("KDV'li fiyat : " + priceWithVat);
+            System.out.println("KDV tutarı : " + (priceWithVat - priceWithoutVat));
+        }catch (Exception e){
+            System.out.println("Lütfen double türünde bir değer giriniz!");
+            scanner.nextDouble();
+        }
+    }
+}
