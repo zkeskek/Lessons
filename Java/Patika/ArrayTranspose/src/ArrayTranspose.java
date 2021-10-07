5f808edf1b10fd862eef6afe5307907503156f78
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-07 12:05:14 +0300

    subarray ilavesi

--- Java/Patika/ArrayTranspose/src/ArrayTranspose.java
+++ Java/Patika/ArrayTranspose/src/ArrayTranspose.java
@@ -1,9 +1,54 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayTranspose {
 
     // https://app.patika.dev/moduller/java101/odev-array-transpose
+
+    static int[] input;
+    static int loop;
+
     public static void main(String[] args) {
+
+        transpose();
+
+        // negatif toplam içeren alt kümelerin sayısını verir.
+        //subArrayInit();
+
+    }
+
+    public static void subArrayInit(){
+        Scanner scanner = new Scanner(System.in);
+
+        // n değerini al
+        input = new int[scanner.nextInt()];
+
+        // diziyi oluştur
+        for(int i = 0; i < input.length; i++){
+            input[i] = scanner.nextInt();
+        }
+
+        // alt diziler içinden negatifler kombinasyonlarını bul
+        loop = subArray(0);
+
+        System.out.println(loop);
+    }
+
+    public static int subArray(int index){
+        int sum = 0;
+        for(int i = index; i < input.length; i++){
+            sum += input[i];
+            if(sum < 0){
+                loop++;
+            }
+        }
+        if(index < input.length){
+            return subArray(index +1);
+        }
+        return loop;
+    }
+
+    public static void transpose(){
         int[][] myArray = {
                 {2,3,4},
                 {5,6,4}
@@ -20,6 +65,5 @@ public class ArrayTranspose {
         for (int i = 0; i < transpose.length; i++) {
             System.out.println(Arrays.toString(transpose[i]));
         }
-
     }
 }
