e4c6df74a0acc44a4d1104f88a361314c75e45b2
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-24 01:15:13 +0300

    calculator

---
+++ Java/Patika/Calculator/src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    // https://app.patika.dev/moduller/java-ile-backend-web-development-projeleri/hesap-makinasi
+    public static void main(String[] args) {
+
+        /*
+        4 temel işlem (toplama,çıkarma,çarpma,bölme)
+Üs alma
+Karekök alma
+Türev alma (kullanıcıdan katsayıları ve denklem derecesini almanız yeterlidir) - Integral Alma (kullanıcıdan katsayıları ve denklem derecesini almanız yeterlidir) - Faktöriyel Hesaplama
+Mod Alma
+Mutlak Değer Hesaplama
+         */
+    }
+}
