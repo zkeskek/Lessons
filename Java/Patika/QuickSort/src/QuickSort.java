d2335616d2dc14222ad0e4a5c6f6d75f3d90d162
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-24 01:12:16 +0300

    quick sort

---
+++ Java/Patika/QuickSort/src/QuickSort.java
@@ -0,0 +1,9 @@
+public class QuickSort {
+
+    // https://app.patika.dev/moduller/java-ile-backend-web-development-projeleri/quick-sort
+    public static void main(String[] args) {
+
+        // TODO Java’da Quick Sort (Hızlı Sıralama) algoritmasını uygulayacak Java sınıfı yazın.
+
+    }
+}
--- Java/Patika/TourismAgency/src/TourismAgency.java
+++ Java/Patika/TourismAgency/src/TourismAgency.java
@@ -3,5 +3,8 @@ public class TourismAgency {
     // https://app.patika.dev/moduller/java-102/odev-turizm-acente
     public static void main(String[] args) {
 
+       String abc = "a";
+
+        abc.substring(0).toUpperCase();
     }
 }
