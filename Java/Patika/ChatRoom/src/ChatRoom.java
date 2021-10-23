7e4c180377afae2fbd43786bfad75821b77202de
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-24 01:21:08 +0300

    chat room

---
+++ Java/Patika/ChatRoom/src/ChatRoom.java
@@ -0,0 +1,20 @@
+public class ChatRoom {
+
+    // https://app.patika.dev/moduller/java-ile-backend-web-development-projeleri/chat-odas%C4%B1-uygulamas%C4%B1
+    public static void main(String[] args) {
+
+        /*
+            Java’da komut satırından başlatılan bir konsol Chat uygulaması yapınız.
+            Sunucu (server) ve kullanıcı (client) aynı ağdaki farklı bilgisayarlarda çalışabilir, örn. Yerel Alan Ağı (LAN).
+            Sohbet odası oluşturmalı.
+            Sohbet odasına birden fazla kullanıcı bağlanabilir Herkes birbirinin yazdığı mesajı okuyabilir.
+
+
+            Sunucuya bağlandıktan sonra, sohbete girmek için kullanıcının adını girmesi gerekir. Sunucu, yeni kullanıcıya şu anda çevrimiçi olan kullanıcıların bir listesini gönderir. - Yeni bir kullanıcı sohbete katıldığında ve bir kullanıcı sohbetten ayrıldığında, her kullanıcı bilgilendirilir.
+            Uygulama sunucu ve istemci olmak üzere iki bölümden oluşmaktadır. Her bölüm ayrı bilgisayarlarda bağımsız olarak çalışabilir.
+
+            Socket
+            Thread
+         */
+    }
+}
