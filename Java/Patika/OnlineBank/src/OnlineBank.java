c23f57e493b7e020f4b4d4d781979180c7ea30f7
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-24 01:17:07 +0300

    online bank

---
+++ Java/Patika/OnlineBank/src/OnlineBank.java
@@ -0,0 +1,26 @@
+public class OnlineBank {
+
+    // https://app.patika.dev/moduller/java-ile-backend-web-development-projeleri/online-banka-sistemi
+    public static void main(String[] args) {
+
+        /*
+        Java’da bankacılıkta yapılan işlemlerin aynı görevi yapan “Online Banka Sistemi” projesi yapmak.
+
+
+
+Projede Banka ve Kullanıcılar olacak.
+Kullanıcılar sisteme T.C. ve şifre ile giriş yapacak.
+Şifre içerisinde doğum tarihi olmayacak.
+Kullanıcılar birbirine para transfer yapabilecek.
+Kullanıcı bankaya kredi ödemesi veya kredi kartı ekstresi ödeyebilecek
+
+
+Kullanılması gereken teknikler:
+
+
+
+Veritabanı kullanılmayacaktır, veriler json formatında proje içerisinde okunup işlenecektir.
+Proje sınıflar kullanılarak yapılmalıdır.
+         */
+    }
+}
