5fc68c426d544b6068868bbf837b283860e76823
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-10-24 01:19:17 +0300

    rent a car

---
+++ Java/Patika/RentACar/src/RentACar.java
@@ -0,0 +1,24 @@
+public class RentACar {
+
+    // https://app.patika.dev/moduller/java-ile-backend-web-development-projeleri/ara%C3%A7-kiralama-portal%C4%B1
+    public static void main(String[] args) {
+
+        /*
+            Sisteme araç kiralayan firmalar eklenecek.
+            Firmalar bulundukları şehri sisteme girmelidir.
+            Firmalar kendilerine ait araçları sisteme girecekler
+            Araçların günlük fiyatlarını ve fiyatların geçerli olduğu tarih aralıklarını
+            Aracın tipini (binek, arazi , ticari araçlar vs.)
+            Hangi tarihlerde müsait olduklarını
+            Varsa ek hizmetlerini ve fiyatlarını tanımlamalılar.
+            Kullanıcılar sisteme kaydolmalılar.
+            Kullanıcılar sistemde şehirler üzerinden araç tipine ve tarihe göre uygun araçları listeleyebilmeli ve rezervasyon yapabilmeli.
+            Rezervasyon yapılan tarihte ilgili aracın müsait olması ve başka kiralama ile çakışmaması gerekmektedir.
+            Firmalar ve kullanıcılar yaptıkları rezervasyonları listeleyebilmeliler.
+            Rezervasyonlar en erken 1 gün öncesinden iptal edilebilir.
+
+            Veritabanı (DATABASE)
+            Kullanıcı Arayüzü (GUI)
+         */
+    }
+}
