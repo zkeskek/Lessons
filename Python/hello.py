1b367278c3acc55e03ab7ae4fd87ca8140150fcc
Mesut GENEZ <mesut.genez@imovasyon.com>
2021-11-15 20:46:13 +0300

    patika odev 1

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,27 @@
-print("Hello Python")
+list1 = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
+list2 = [[1, 2], [3, 4], [5, 6, 7]]
+
+
+def def1(list1):
+    for l in list1:
+        if type(l) == list:
+            def1(l)
+        else:
+            print(l, end=" ")
+
+
+def def2(list2):
+    newList = list()
+    for l in list2:
+        if type(l) == list:
+            l.reverse()
+            newList.append(l)
+        else:
+            l.reverse()
+            newList.append(l)
+    print(newList)
+
+
+def1(list1)
+print()
+def2(list2)
