1ea44eae2c3ca0767a86eb6e09b4c23fc42ee68d
Mesut Genez <mstgnz@mst.local>
2022-06-12 23:56:55 +0300

    minefield game

---
+++ Go/MineField/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	rowCount    int
+	columnCount int
+	level       int
+	row         int
+	column      int
+	err         error
+)
+
+func main() {
+
+	fmt.Println(`WELCOME TO MINEFIELD GAME
+- Set game board boundaries.
+- Give a value for the degree of difficulty.
+- The hardest value is 2.
+- Start the game by selecting the row and column value.
+-----------------------------------------------------`)
+
+	fmt.Print("Row Count : ")
+	_, err = fmt.Scanf("%d", &rowCount)
+	fmt.Print("Column Count :")
+	_, err = fmt.Scanf("%d", &columnCount)
+	fmt.Print("Level : ")
+	_, err = fmt.Scanf("%d", &level)
+
+	if err != nil || rowCount <= 0 || columnCount <= 0 || level <= 0{
+		fmt.Println("Please just positive number")
+		os.Exit(0)
+	}
+
+	// max 20x20
+	if rowCount > 20 || columnCount > 20 {
+		rowCount = 20
+		columnCount = 20
+	}
+
+	// min 3x3
+	if rowCount < 3 || columnCount < 3 {
+		rowCount = 3
+		columnCount = 3
+	}
+
+	// init minefield
+	mine := Mine{
+		rowCount,
+		columnCount,
+		level,
+	}
+
+	mine.Create()
+
+	// start
+	for {
+		if mine.isWin() {
+			fmt.Println("Congratulations you are win")
+			os.Exit(0)
+		}
+		mine.Print()
+		fmt.Print("Row : ")
+		_, err = fmt.Scanf("%d", &row)
+		fmt.Print("Column :")
+		_, err = fmt.Scanf("%d", &column)
+
+		if err != nil || row <= 0 || column <= 0 {
+			fmt.Println("Please just positive number")
+			os.Exit(0)
+		}
+
+		mine.Choose(row, column)
+	}
+
+}
---
+++ Go/MineField/mine.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+var (
+	mineBool  [][]int
+	mineBoard [][]string
+	mineCount int
+	mineOpen  int
+)
+
+type Mine struct {
+	RowCount    int
+	ColumnCount int
+	Level       int
+}
+
+func (m *Mine) Create() {
+	// Set Board
+	mineBoard = make([][]string, m.RowCount)
+	mineBool = make([][]int, m.RowCount)
+	for i := 0; i < m.RowCount; i++ {
+		mineBoard[i] = make([]string, m.ColumnCount)
+		mineBool[i] = make([]int, m.ColumnCount)
+	}
+	for i := 0; i < m.RowCount; i++ {
+		for j := 0; j < m.ColumnCount; j++ {
+			mineBoard[i][j] = "__"
+			mineBool[i][j] = 0
+		}
+	}
+	// Set mine
+	mineCount = m.RowCount * m.ColumnCount / m.Level
+	mineOpen = mineCount
+	row, column, mine := 0, 0, 0
+	for i := 0; i < mineCount; i++ {
+		row = rand.Intn(m.RowCount)
+		column = rand.Intn(m.ColumnCount)
+		if mineBool[row][column] == 0 {
+			mineBool[row][column] = 1
+			mine++
+		} else {
+			if mine != mineCount {
+				i--
+			}
+		}
+	}
+}
+
+func (m *Mine) Choose(row, column int) {
+	// start index 1 for user
+	row--
+	column--
+	// selected index is available and check bomb index
+	if m.isIndexAvailable(row, column) {
+		m.isBomb(row, column)
+		m.setBoard(row, column)
+	}
+}
+
+func (m *Mine) isIndexAvailable(row, column int) bool {
+	if m.RowCount > row && m.ColumnCount > column {
+		if mineBoard[row][column] == "__" {
+			return true
+		} else {
+			fmt.Println("Already choose this index!")
+		}
+	}
+	return false
+}
+
+func (m *Mine) isBomb(row, column int) {
+	if mineBool[row][column] == 1 {
+		fmt.Println("BOOOMMBBBBBB")
+		fmt.Println("GAME OVER!")
+		os.Exit(0)
+	}
+}
+
+func (m *Mine) setBoard(row, column int) {
+	count, row1, row2, column1, column2 := 0, row, row, column, column
+	if row > 0 {
+		row1 = row - 1
+	}
+	if column > 0 {
+		column1 = column - 1
+	}
+	if len(mineBool) > row {
+		row2 = row + 1
+	}
+	if len(mineBool[0]) > column+1 {
+		column2 = column + 1
+	}
+	for i := row1; i <= row2; i++ {
+		for j := column1; j <= column2; j++ {
+			if mineBool[i][j] == 1{
+				mineCount++
+			}
+		}
+	}
+	mineOpen += 1
+	mineBoard[row][column] = string(rune(count))
+}
+
+func (m *Mine) isWin() bool {
+	return mineOpen == m.RowCount*m.ColumnCount
+}
+
+func (m *Mine) Print() {
+	for i := 0; i < m.RowCount; i++ {
+		for j := 0; j < m.ColumnCount; j++ {
+			fmt.Print(mineBoard[i][j])
+		}
+		fmt.Println()
+	}
+}
