7c38903927334a3b88980a8fbf5d8a92a76ee980
mstgnz <mesutgenez@hotmail.com>
2022-12-05 23:14:46 +0300

    grpc web

--- Go/grpc-angular/server/main.go
+++ Go/grpc-angular/server/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"grpc-demo/proto"
 	"net"
+	"net/http"
+
+	"grpc-demo/proto"
 
 	"google.golang.org/grpc"
 )
 
-func main(){
+func main() {
 
 	listen, err := net.Listen("tcp", "localhost:9090")
 	if err != nil {
@@ -25,10 +27,26 @@ func main(){
 
 }
 
-type helloController struct{
+type helloController struct {
 	proto.UnimplementedHelloServiceServer
 }
 
 func (h *helloController) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
-	return &proto.HelloResponse{Message: req.Name}, nil
+	return &proto.HelloResponse{Message: req.Name + "ben serverdan geldim :)"}, nil
+}
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // change this later
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(204)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
